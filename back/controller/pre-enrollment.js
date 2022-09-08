fecae75fc8099c6e6ba0fed2a4502cc01ec6aec9
mathmartins2 <dev.sincroni@gmail.com>
2022-09-08 17:42:58 -0300

    a

--- back/controller/pre-enrollment.js
+++ back/controller/pre-enrollment.js
@@ -99,9 +99,9 @@ export const insertNewPreEnrollment = async (req, res) => {
 
         files.map(async (file) => {
             let path = file.path.replace(/\\/g, '/');
-            path = path.split('/').slice(3).join('/');
+            path = path.split('/').slice(4).join('/');
 
-            const sql = `INSERT INTO prematricula_documentos_fundaj (id_prematricula, nome_documento, caminho_documento, id_documento_prematricula) VALUES (${id}, '${file.filename}', '${path}', ${file.fieldname})`;
+            const sql = `INSERT INTO prematricula_documentos_fundaj (id_prematricula, nome_documento, caminho_documento, id_documento_prematricula) VALUES (${id}, '${file.filename}', '${'../' + path}', ${file.fieldname})`;
 
             await pool.query(sql);
         });
