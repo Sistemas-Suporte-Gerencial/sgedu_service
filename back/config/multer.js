642d9017a55eb8c9bd07c06ef0e81d8705e890d7
mathmartins2 <dev.sincroni@gmail.com>
2022-09-08 13:24:59 -0300

    a

--- back/config/multer.js
+++ back/config/multer.js
@@ -22,7 +22,7 @@ export const storage = diskStorage({
         randomBytes(16, (err, hash) => {
             if (err) cb(err);
     
-            file.key = `${hash.toString("hex")}-${file.originalname.trim()}`;
+            file.key = `${hash.toString("hex")}-${file.originalname.replace(/ /g,'')}`;
     
             cb(null, file.key);
         });
